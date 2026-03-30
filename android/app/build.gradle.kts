70c0262006ed2e331728ae0fd8953d933f812a55
comerciojuan <paseodelcomerciojuan@gmail.com>
2026-03-30 15:18:38 -0500

    fix: sha256 actualizado

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,21 +29,28 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.paseodelcomercio.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion  // Required for App Links (autoVerify)
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("debug_release.keystore")
+            storePassword = project.property("KEYSTORE_STORE_PASSWORD") as String
+            keyAlias = project.property("KEYSTORE_ALIAS") as String
+            keyPassword = project.property("KEYSTORE_KEY_PASSWORD") as String
+        }
+    }
+
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("release")
+        }
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
